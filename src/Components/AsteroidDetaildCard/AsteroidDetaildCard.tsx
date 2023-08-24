a3cebd2080300ea0c9f30d28adb2dc31f85cf253
viacheslav.orlov <viacheslavorlov@gmail.com>
2023-08-24 21:57:17 +0300

    исправлен AsteroidDetaildCard.tsx нужно поправить дизайн, больше тестов, и документация

--- src/Components/AsteroidDetaildCard/AsteroidDetaildCard.tsx
+++ src/Components/AsteroidDetaildCard/AsteroidDetaildCard.tsx
@@ -21,7 +21,7 @@ export const AsteroidDetailed = (props: AsteroidDetailedCardPrors) => {
         estimated_diameter,
         is_potentially_hazardous_asteroid,
         close_approach_data,
-        nasa_jpl_url
+        nasa_jpl_url,
     } = asteroid
     const closestDate = findClosestDate(close_approach_data)
 
@@ -29,20 +29,14 @@ export const AsteroidDetailed = (props: AsteroidDetailedCardPrors) => {
         <div className={classNames(cls.AsteroidDetailedCard, className)}>
             <h2 className={cls.name}> Астероид: {name}</h2>
             <div className={cls.asteroidData}>
-                <div className={cls.distance}>
-                    <div data-testid={'distance-type'} className={cls.distanceText}>
-                        {Math.round(Number(distanceType === 'km' ? distanceKm : distanceLunar)) + distanceIdentifier}
-                    </div>
-                    <Image className={cls.distanceArrow} src={'/Arrow.svg'} width={130} height={5} alt={'стрелка'}/>
+                <div className={cls.diameter}>
+                    <div>Максимальный диаметр: Ø {Math.round(estimated_diameter.meters.estimated_diameter_max)} м</div>
                 </div>
-                <Image src={'/asteroid.png'} alt={'астероид'} height={40} width={40}/>
-                <div className={cls.AsteroidName}>
-                    <Link href={`/asteroid-page/${id}`} className={cls.name}>{name}</Link>
-                    <div>Ø {Math.round(estimated_diameter.meters.estimated_diameter_max)} м</div>
-                </div>
-
             </div>
             <div className={cls.closestDate}>Ближайшая дата подлета: {closestDate}</div>
+            <div className={cls.hazard}>
+                {is_potentially_hazardous_asteroid && <Image src={'/warning.svg'} alt={'Опасен'} width={130} height={30}/>}
+            </div>
 
         </div>
     );
